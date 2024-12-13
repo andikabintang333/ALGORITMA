81195007ae0c99b3fd63c7b5bf46f155d74f2013
andikabintang333 <andkabntng@gmail.com>
2024-12-13 14:44:48 +0700

    Add files via upload

---
+++ Variabel_int.cpp
@@ -0,0 +1,15 @@
+#include <iostream.h>
+#include <conio.h>
+int main ()
+{
+// declaring variables:
+int a, b;
+int result;
+// process:
+a = 5;
+b = 2;
+result = a / b;
+cout << "Hasil (int) = " << result;
+getch();
+return 0;
+}
